1e3e568a23359ef26e2f3e18328e30cc855938a3
mug3tsu <mug3tsu92@gmail.com>
2013-12-08 16:34:05 +0100

    Initial map commit

--- include/map.h
+++ include/map.h
@@ -1,21 +1,44 @@
 #ifndef MAP_H
 #define MAP_H
 
+#include <fstream>
+#include <string>
+
 #include <SFML/Graphics.hpp>
+
+enum FieldState {
+    Empty = 0,
+    Obstacle = 1,
+    Player = 2,
+    Enemy = 3,
+    Item = 4
+};
+
+struct Field {
+    FieldState state;
+    int id;
+};
 
 class Map {
 public:
-    Map(short width, short height, std::string image);
+    Map(std::string filename);
     ~Map();
+    void load(std::string filename);
+    bool isLoaded();
     void showGrid(bool visible);     // turn on/off grid on map
     void setMapSize(short width, short height);      // set map size (in tiles)
     short getMapWidth();
     short getMapHeight();
+    void draw();
 private:
     short width;
     short height;
-    bool gridVisible;
-    sf::Texture image;
+    bool gridVisible = false;
+    bool loaded = false;
+    sf::Texture mapImage;
+    sf::Sprite mapSprite;
+
+    Field** map;
 
     Map();
 protected:
--- src/map.cpp
+++ src/map.cpp
@@ -5,13 +5,53 @@ Map::Map() {
 
 }
 
-Map::Map(short width, short height, std::string image) {
-    this->setMapSize(width,height);
-    this->gridVisible = false;
+Map::Map(std::string filename) {
+    this->load(filename);
+}
 
-    if (!this->image.loadFromFile(image)) {
-        // ERROR
-    }    
+void Map::load(std::string filename) {
+    std::ifstream mapFile;
+    mapFile.open(filename);
+    if(mapFile.good()) {
+        mapFile >> this->width >> this->height;
+
+        // utworzenie dynamicznej tablicy
+        this->map = new Field* [width];
+        for (int i = 0; i < width; i++) {
+            map[i] = new Field [height];
+        }
+        // wypełnienie jej pustymi polami
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                map[i][j].state = Empty;
+                map[i][j].id = 0;
+            }
+        }
+        // wczytanie grafiki mapy
+        std::string temp;
+        getline(mapFile,temp);
+        if (!this->mapImage.loadFromFile(temp)) {
+            // ERROR
+        }
+        this->mapSprite.setTexture(this->mapImage);
+        // wczytanie zawartości mapy
+        int x,y,state,id;
+        while (!mapFile.eof()) {
+            mapFile >> x >> y >> state >> id;
+            map[x][y].state = (FieldState)state;
+            map[x][y].id = id;
+        }  
+
+    } else {
+        // BŁĄD
+        return;
+    }
+    mapFile.close();
+    this->loaded = true;
+}
+
+bool Map::isLoaded() {
+    return this->loaded;
 }
 
 void Map::showGrid(bool visible) {
@@ -31,3 +71,11 @@ short Map::getMapHeight() {
     return this->height;
 }
 
+void Map::draw() {
+    //sf::RenderWindow *w = engine->getWindow();
+    //w->draw(this->mapSprite);
+    if (this->gridVisible) {
+        sf::Color c(255,255,255,128);
+        
+    }
+}
